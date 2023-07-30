2cef54dbab794fca2e2091f1b50ca7eff5065121
Nehal Garg <nehalgarg37@gmail.com>
2023-07-31 03:44:25 +0530

    Changes-Installation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import Header from './Header/Header';
 import Main from './Main/Main';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Login from './login/Login';
+
 function App() {
   return (
-    <div className="App">
-     <Header />
-     <Main />
-     {/* <Login /> */}
-    </div>
+    <Router>
+      
+        <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I am a checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Main />
+          </Route>
+
+          {/* <Login /> */}
+          </Switch>
+        </div>
+     
+    </Router>
   );
 }
 
