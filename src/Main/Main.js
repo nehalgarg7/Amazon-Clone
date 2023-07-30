eb08227391d9ffc78ec5b9c18381d57ad11f081c
Nehal Garg <nehalgarg37@gmail.com>
2023-07-31 02:17:42 +0530

    Banner-Main-Component

---
+++ src/Main/Main.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Main(){
+    return(
+        <div className="main">
+            <div className="main-banner">
+                <img src="https://images-eu.ssl-images-amazon.com/images/G/31/Events/img23/Aug23ART/AugART23_T2_PC_Header_EN_UNREC.jpg" alt="" />
+            </div>
+            <div className="main-row">
+                {/* <Product /> */}
+                {/* <Product /> */}
+            </div>
+            <div className="main-row">
+                {/* <Product /> */}
+                {/* <Product /> */}
+                {/* <Product /> */}
+            </div>
+            <div className="main-row">
+                {/* <Product /> */}
+            </div>
+        </div>
+    )
+}
+
+export default Main
