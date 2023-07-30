6580773f5ff3ba56c27130bc974fa8353ca6ff3b
Nehal Garg <nehalgarg37@gmail.com>
2023-07-31 02:17:48 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header/Header';
+import Main from './Main/Main';
 import Login from './login/Login';
 function App() {
   return (
     <div className="App">
      <Header />
+     <Main />
      {/* <Login /> */}
     </div>
   );
